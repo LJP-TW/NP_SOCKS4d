41db9ec748a91eabd95942d38551373a4f42b1c4
LJP-TW <accr94238@gmail.com>
2021-12-22 19:19:05 +0800

    feat: hw4.cgi ok

--- cgi_dir/src/hw4.cpp
+++ cgi_dir/src/hw4.cpp
@@ -15,6 +15,8 @@
 #include <memory>
 #include <utility>
 #include <vector>
+#include <sys/socket.h>
+#include <arpa/inet.h>
 #include <boost/asio.hpp>
 #include <boost/algorithm/string/classification.hpp>
 #include <boost/algorithm/string/split.hpp>
@@ -22,6 +24,68 @@
 
 using tcp = boost::asio::ip::tcp;
 using namespace std;
+
+typedef unsigned char BYTE;
+typedef unsigned short WORD;
+typedef unsigned int DWORD;
+
+static DWORD ip_to_dword(string ip) {
+  struct sockaddr_in sa;
+  
+  inet_pton(AF_INET, ip.c_str(), &(sa.sin_addr));
+  return sa.sin_addr.s_addr;
+}
+
+static WORD int_to_port(int port) {
+  return ((port & 0xff00) >> 8) | ((port & 0xff) << 8);
+}
+
+struct SOCKS4_REQUEST {
+  SOCKS4_REQUEST() {
+  }
+
+  SOCKS4_REQUEST(tcp::endpoint endpoint) {
+    // CONNECT
+    vn = 4;
+    cd = 1;
+    dstport = int_to_port(endpoint.port());
+    dstip = ip_to_dword(endpoint.address().to_string());
+    nullb = 0;
+  }
+
+  BYTE vn;
+  BYTE cd;
+  WORD dstport;
+  DWORD dstip;
+  BYTE nullb;
+};
+
+struct SOCKS4_REPLY {
+  SOCKS4_REPLY() {
+  }
+
+  SOCKS4_REPLY(char *data) {
+    vn = data[0];
+    cd = data[1];
+    dstport = int_to_port(*((WORD *)&data[2]));
+    dstip = *((DWORD *)&data[4]);
+  }
+
+  BYTE vn;
+  BYTE cd;
+  WORD dstport;
+  DWORD dstip;
+};
+
+struct socks_info {
+  socks_info() {
+    enable = 0;
+  }
+  
+  int enable;
+  string hostname;
+  string port;
+};
 
 struct connect_info {
   connect_info() {
@@ -38,11 +102,12 @@ class client
   : public std::enable_shared_from_this<client>
 {
 public:
-  client(boost::asio::io_context& io_context, connect_info info)
+  client(boost::asio::io_context& io_context, connect_info info, socks_info socks_setting)
     : resolver_(boost::asio::make_strand(io_context)),
       socket_(io_context)
   {
     info_ = info;
+    socks_setting_ = socks_setting;
     
     // Read testcase
     string filename = "./test_case/" + info_.testcasename;
@@ -59,6 +124,9 @@ public:
     }
 
     cerr << "[^] Constructor (" << info_.server << "," << info_.hostname << "," << info_.port << ")" << endl;
+    if (socks_setting_.enable == 1) {
+      cerr << "[^]\tuse SOCKS (" << socks_setting_.hostname << "," << socks_setting_.port << ")" << endl;
+    }
   }
 
   void start()
@@ -67,9 +135,45 @@ public:
   }
 
 private:
+  void debug_dump(char *data, int length) {
+    int cnt = 0;
+    int i = 0;
+    cerr << "[debug] Length: " << length << endl;
+    for (; i < length; ++i) {
+      fprintf(stderr, "%02x ", (BYTE)data[i]);
+      cnt += 1;
+      if (cnt == 0x10) {
+        fprintf(stderr, " | ");
+        for (; cnt; cnt--) {
+          if (32 <= data[i + 1 - cnt] && data[i + 1 - cnt] <= 127) {
+            fprintf(stderr, "%c ", data[i + 1 - cnt]);
+          } else {
+            fprintf(stderr, ". ");
+          }
+        }
+        cerr << endl;
+      }
+    }
+    if (length % 0x10) {
+      for (int j = 0x10 - (length % 0x10); j; --j) {
+        fprintf(stderr, "-- ");
+      }
+      fprintf(stderr, " | ");
+    }
+    for (; cnt; cnt--) {
+      if (32 <= data[i - cnt] && data[i - cnt] <= 127) {
+        fprintf(stderr, "%c ", data[i - cnt]);
+      } else {
+        fprintf(stderr, ". ");
+      }
+    }
+    cerr << endl;
+  }
+
   void do_resolve()
   {
     auto self(shared_from_this());
+
     resolver_.async_resolve(
       string(info_.hostname),
       string(info_.port),
@@ -83,9 +187,56 @@ private:
 
           cerr << "[O] Resolve OK (" << info_.server << "," << info_.hostname << "," << info_.port << "," << endpoint_ << ")" << endl;
 
-          do_connect();
+          do_handle_socks();
+          // do_connect();
         } else {
           cerr << "[O] Resolve failed (" << info_.server << "," << info_.hostname << "," << info_.port << "," << endpoint_ << ")" << endl;
+        }
+      });
+  }
+
+  void do_handle_socks()
+  {
+    auto self(shared_from_this());
+
+    if (socks_setting_.enable) {
+      resolver_.async_resolve(
+        string(socks_setting_.hostname),
+        string(socks_setting_.port),
+        [this, self](boost::system::error_code ec, tcp::resolver::results_type endpoints)
+        {
+          if (!ec) {
+            for (auto it = endpoints.cbegin(); it != endpoints.cend(); it++) {
+              socks_endpoint_ = *it;
+              break;
+            }
+
+            cerr << "[O] SOCKS Resolve OK (" << socks_setting_.hostname << "," << socks_setting_.port << "," << socks_endpoint_ << ")" << endl;
+            do_connect_socks();
+          } else {
+            cerr << "[O] SOCKS Resolve failed (" << socks_setting_.hostname << "," << socks_setting_.port << "," << socks_endpoint_ << ")" << endl;
+            do_connect();
+          }
+        });
+    }
+  }
+
+  void do_connect_socks()
+  {
+    auto self(shared_from_this());
+  
+    socket_.async_connect(
+      socks_endpoint_,
+      [this, self](boost::system::error_code ec)
+      {
+        if (!ec) {
+          cerr << "[O] Connect OK (" << socks_setting_.hostname << "," << socks_setting_.port << ")" << endl;
+          
+          // Send SOCKS4_REQUEST
+          do_send_socks4_request();   
+        } else {
+          cerr << "[X] Connect failed (" << socks_setting_.hostname << "," << socks_setting_.port << ")" << endl;
+          do_connect();
         }
       });
   }
@@ -93,6 +244,7 @@ private:
   void do_connect()
   {
     auto self(shared_from_this());
+  
     socket_.async_connect(
       endpoint_,
       [this, self](boost::system::error_code ec)
@@ -102,6 +254,61 @@ private:
           do_read();
         } else {
           cerr << "[X] Connect failed (" << info_.server << "," << info_.hostname << "," << info_.port << ")" << endl;
+        }
+      });
+  }
+
+  void do_send_socks4_request() 
+  {
+    auto self(shared_from_this());
+
+    SOCKS4_REQUEST req(endpoint_);
+
+    boost::asio::async_write(socket_, boost::asio::buffer((char *)&req.vn, 9),
+      [this, self](boost::system::error_code ec, std::size_t /*length*/)
+      {
+        if (!ec) {
+          cerr << "[O] SOCKS4_REQUEST send OK (" << socks_setting_.hostname << "," << socks_setting_.port << ")" << endl;
+          do_read_socks4_reply();
+        } else {
+          cerr << "[x] SOCKS4_REQUEST send failed (" << socks_setting_.hostname << "," << socks_setting_.port << ")" << endl;
+        }
+      });
+  }
+
+  void do_read_socks4_reply() 
+  {
+    auto self(shared_from_this());
+
+    socket_.async_read_some(boost::asio::buffer(data_, 8),
+      [this, self](boost::system::error_code ec, size_t length)
+      {
+        if (!ec) {
+          cerr << "[O] SOCKS4_REPLY Read OK (" << socks_setting_.hostname << "," << socks_setting_.port << ")" << endl;
+          
+          debug_dump(data_, length);
+
+          if (length != 8) {
+            cerr << "[x] SOCKS4_REPLY Read failed: Length error (" << socks_setting_.hostname << "," << socks_setting_.port << ")" << endl;
+            return;
+          }
+
+          SOCKS4_REPLY reply(data_);
+
+          if (reply.vn != 0) {
+            cerr << "[x] SOCKS4_REPLY Read failed: VN error (" << socks_setting_.hostname << "," << socks_setting_.port << ")" << endl;
+            return;
+          }
+
+          if (reply.cd != 90) {
+            cerr << "[x] SOCKS4_REPLY Read failed: SOCKS4 Server rejected (" << socks_setting_.hostname << "," << socks_setting_.port << ")" << endl;
+            return;
+          }
+
+          // OK, SOCKS4 connection established
+          do_read();
+        } else {
+          cerr << "[x] SOCKS4_REPLY Read failed (" << socks_setting_.hostname << "," << socks_setting_.port << ")" << endl;
         }
       });
   }
@@ -217,7 +424,9 @@ private:
   tcp::resolver resolver_;
   tcp::socket socket_;
   connect_info info_;
+  socks_info socks_setting_;
   tcp::endpoint endpoint_;
+  tcp::endpoint socks_endpoint_;
   vector<string> testcase_;
 };
 
@@ -227,6 +436,7 @@ int main()
   {
     vector<string> params;
     vector<connect_info> infos(5);
+    socks_info socks_setting;
     string index_page = R""""(
 <!DOCTYPE html>
 <html lang="en">
@@ -309,16 +519,35 @@ int main()
         string key = param.substr(0, split_idx);
         string value = param.substr(split_idx + 1);
         
+        int socks = 0;
         int idx = -1;
         
         if (key.length() == 2) {
-          int n = key[1] - '0';
-          if (0 <= n && n < 5) {
-            idx = n;
+          if (key[0] == 's') {
+            // For SOCKS setting
+            socks = 1;
+            socks_setting.enable = 1;
+          } else {
+            int n = key[1] - '0';
+
+            if (0 <= n && n < 5) {
+              idx = n;
+            }
           }
         }
 
-        if (idx != -1) {
+        if (socks == 1) {
+          switch (key[1]) {
+            case 'h':
+              // Assign SOCKS host
+              socks_setting.hostname = value;
+              break;
+            case 'p':
+              // Assign SOCKS port
+              socks_setting.port = value;
+              break;
+          }
+        } else if (idx != -1) {
           switch (key[0]) {
             case 'h':
               infos[idx].hostname = value;
@@ -362,7 +591,7 @@ int main()
     for (auto info : infos) {
       if (info.hostname != "") {
         cerr << "[C] (" << info.server << "," << info.hostname << "," << info.port << ")" << endl;
-        make_shared<client>(io_context, info)->start();
+        make_shared<client>(io_context, info, socks_setting)->start();
       }
     }
 
